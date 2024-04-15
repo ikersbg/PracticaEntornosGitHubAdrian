be88a5e29f5e18e3de6cef26d6e3bb91ceae8060
ikersbg <iker.san7@educa.madrid.org>
2024-04-15 19:02:16 +0200

    Commit de javadoc

--- src/biblioikersan7/paco.java
+++ src/biblioikersan7/paco.java
@@ -1,7 +1,28 @@
+
 package biblioikersan7;
 
+/**
+ * @author iker
+ * @version 1.5
+ * @since 1.0
+ * */
 public class paco {
     public static void main(String[] args) {
         System.out.println("Hola mundote");
+    }
+    /**
+     * Esta funcion sirve para los comentarios
+     * @param primero vale para hacer el primer comentario
+     * @return primero
+     * @since 1.1
+     * */
+    public static double comentarios(double primero){
+        return primero;
+    }
+    /**
+     * @deprecated
+     * */
+    public static String destruirLibro(String libro){
+        return libro;
     }
 }
